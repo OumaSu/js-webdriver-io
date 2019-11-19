90fb62ccda7d3968fa03f22931d28f66d49bf415
s.kazantsev <s.kazantsev@tinkoff.ru>
2019-11-19 12:11:53 +0500

    prettier

--- test/specs/sampleTests.js
+++ test/specs/sampleTests.js
@@ -1,5 +1,5 @@
-const assert = require('assert')
+const assert = require("assert");
 
-describe('sample test', () => {
-    it('test name', () => { })
-})
+describe("sample test", () => {
+  it("test name", () => {});
+});
